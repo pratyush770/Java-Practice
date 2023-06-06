c9ee2658c939ad4d3636f1bb621ce50ec81ade91
pratyush770 <pratyushmajumdar27@gmail.com>
2023-06-06 15:31:15 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;  // for taking input from the user
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("JAVA Practice");
+
+
+        // Literals in JAVA
+//        int a=10;
+//        byte b=20;
+//        short c=1233;
+//        long d=55555L;
+//        char e='A';
+//        boolean f=true;
+//        float g=2.56F;
+//        double h=2.6666;
+//        String i="Hello"; //extra literal provided by java
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+//        System.out.println(d);
+//        System.out.println(e);
+//        System.out.println(f);
+//        System.out.println(g);
+//        System.out.println(h);
+//        System.out.println(i);
+
+
+        // Addition of two numbers by taking input from the user
+//        Scanner sc=new Scanner(System.in);
+//        System.out.print("Enter the first number : ");
+//        int a=sc.nextInt();
+//        System.out.print("Enter the second number : ");
+//        int b=sc.nextInt();
+//        int sum=a+b;
+//        System.out.print("The addition of the two numbers is : ");
+//        System.out.println(sum);
+//        System.out.print("Enter the string : ");
+//        String str=sc.nextLine(); //for reading all the words and for reading only 1 word use only next();
+//        System.out.println(str);
+
+
+        // Calculating percentage for 5 subjects by taking inputs from the user
+//        Scanner sc=new Scanner(System.in);
+//        System.out.print("Enter the marks of the first subject : ");
+//        float a=sc.nextFloat();
+//        System.out.print("Enter the marks of the second subject : ");
+//        float b=sc.nextFloat();
+//        System.out.print("Enter the marks of the third subject : ");
+//        float c=sc.nextFloat();
+//        System.out.print("Enter the marks of the fourth subject : ");
+//        float d=sc.nextFloat();
+//        System.out.print("Enter the marks of the fifth subject : ");
+//        float e=sc.nextFloat();
+//        float sum=((a+b+c+d+e)/500)*100;
+//        System.out.print("You got " + sum + "%");
+
+
+        // Increment and Decrement operator usage
+//        int a = 10;  // first value is assigned (10) and then it is incremented
+//        System.out.println(a++); // it will show 10
+//        int b = 12;  // first value is incremented (13) and then it is assigned
+//        System.out.println(++b); // it will show 13
+        // same logic applies for a-- and --b
+
+
+        // Type casting in java
+//        float a = 10.0f + 2;  // float + int = float
+//        int sum= (int) (a); // converts the float into integer
+//        System.out.println(sum); // it shows 12 and not 12.0
+
+
+        // printf usage
+//        float a = 10.123f;
+//        float b = 12.111f;
+//        float c= a+b;
+//        System.out.printf("The sum of the two numbers is : %f",c);  // returns answer with all decimal points
+//        System.out.printf("\nThe sum of the two numbers is : %.2f",c); // returns answer with only 2 decimal points (%.2f)
+//        System.out.printf("\nThe sum of the two numbers is : %8.3f",c); // returns answer in total 8 spaces with only 3 decimal points
+
+    }
+}
